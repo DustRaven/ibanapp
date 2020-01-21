3887e8e947f80605d4abcb24cea8ca3d492f3931
Samuel Wulf <samuel.wulf@liondeer.de>
2020-01-21 11:05:32 +0100

    changes

--- Program.cs
+++ Program.cs
@@ -6,12 +6,42 @@ namespace IBANApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Menu());
+            Menu();
         }
 
         static void GenerateIban()
         {
+            string accountNumber;
+            string bankNumber;
+            string[] iban = new string[4];
 
+            Console.Clear();
+            Console.Write("Bitte geben Sie eine Kontonummer ein: ");
+            accountNumber = Console.ReadLine();
+
+            Console.Write("Bitte die BLZ eingeben: ");
+            bankNumber = Console.ReadLine();
+
+            iban[0] = "DE";
+            iban[1] = "00";
+            iban[2] = bankNumber;
+            iban[3] = accountNumber;
+
+            iban[1] = CalculateChecksum(iban).ToString();
+        }
+
+        static int CalculateChecksum(string[] iban)
+        {
+            char[] countryCode = iban[0].ToCharArray();
+            int[] letterToNumber = new int[2];
+            letterToNumber[0] = countryCode[0] - 55;
+            letterToNumber[1] = countryCode[1] - 55;
+
+            string completeString = iban[2] + iban[3] + letterToNumber[0] + letterToNumber[1] + iban[1];
+            double toCalculate = double.Parse(completeString);
+            double checksum = 98 - (toCalculate % 97);
+
+            return 0;
         }
 
         static void ValidateIban()
@@ -24,9 +54,9 @@ namespace IBANApp
 
         }
 
-        static int Menu()
+        static void Menu()
         {
-            int action;
+            int action = 0;
 
             Console.WriteLine("(1) IBAN aus BLZ und Kontonummer generieren");
             Console.WriteLine("(2) IBAN verifizieren");
@@ -34,14 +64,29 @@ namespace IBANApp
             Console.WriteLine("(4) Beenden");
             Console.Write("Bitte wählen Sie eine Aktion: ");
 
-            Console.ReadLine(out action);
-            if (action > 0 && action < 5)
+            while (action <= 0 || action > 4)
             {
-                return action;
+                bool valid = int.TryParse(Console.ReadLine(), out action);
+                if (!valid)
+                {
+                    Console.WriteLine("Bitte eine Zahl zwischen 1 und 4 eingeben!");
+                }
             }
 
-            Console.WriteLine("Bitte Aktion 1-4 wählen!");
-            Menu();
+            if (action == 1)
+            {
+                GenerateIban();
+            }
+
+            if (action == 2)
+            {
+                ValidateIban();
+            }
+
+            if (action == 3)
+            {
+                ConvertToIban();
+            }
         }
     }
 }
